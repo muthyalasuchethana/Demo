9d1b034af416fe9deb6819ec3333e2a3127e5c75
suchethana <164745672+muthyalasuchethana@users.noreply.github.com>
2024-03-25 13:44:06 +0530

    Create practice_code.py

---
+++ practice_code.py
@@ -0,0 +1,10 @@
+names = ["suchi", "akhila", "rohitha"]
+for name in names:
+    print(name)
+
+numbers = [1, 2, 3, 4, 5]
+squares = []
+for number in numbers:
+    square = number ** 2
+    squares.append(square)
+print(squares)
